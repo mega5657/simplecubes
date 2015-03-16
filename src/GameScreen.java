92ec5a427857702f888e0e5e4a5a9204828f58b9
JTTorres15 <JTTorres15@HS-JTTorres15.chaminet.net>
2015-03-16 16:27:08 -0700

    need to fix resetting on obstacles

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import java.util.Random;
 
 /*
